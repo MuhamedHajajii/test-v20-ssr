b7c460cb2b5d672427224c6472d5b4dc3ace2605
MuhamedHajajii <MuhamedHajajii@outlook.com>
2025-08-09 21:48:48 +0300

    dist

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from '../components/home/home';
+import { About } from '../components/about/about';
+import { Contact } from '../components/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',redirectTo:'home',pathMatch:'full'
+    },
+    {
+        path:'home',component:Home
+    },
+    {
+        path:'about',component:About
+    },
+    {
+        path:'contact',component:Contact
+    }
+];
