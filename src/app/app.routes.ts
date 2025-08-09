856b49ffcb6a6c78ae2e4f1d1621e925800ed8e6
MuhamedHajajii <MuhamedHajajii@outlook.com>
2025-08-09 22:41:37 +0300

    dist

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,28 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path:'',redirectTo:'home',pathMatch:'full'
-    },
-    {
-        path:'home',loadComponent:() => import('../components/home/home').then(m => m.Home)
-    },
-    {
-        path:'about',loadComponent:() => import('../components/about/about').then(m => m.About)
-    },
-    {
-        path:'contact',loadComponent:() => import('../components/contact/contact').then(m => m.Contact)
-    }
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('../components/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('../components/about/about').then((m) => m.About),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('../components/contact/contact').then((m) => m.Contact),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('../components/notfound/notfound').then((m) => m.Notfound),
+  },
 ];
